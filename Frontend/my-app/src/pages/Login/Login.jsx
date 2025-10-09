24de91ae76e7ce9cc3e24ed06c5ef3527fa189dd
Aryavrat <aryavratmishra.iitb@gmail.com>
2025-10-09 18:31:43 +0530

    hello

--- Frontend/my-app/src/pages/Login/Login.jsx
+++ Frontend/my-app/src/pages/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Login.css'
 import { useNavigate } from 'react-router-dom';
+import busImage from '../../assets/bus.jpeg';
 
 const Login = () => {
   const [email, setEmail] = useState('');
